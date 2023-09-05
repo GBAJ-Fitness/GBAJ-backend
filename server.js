493e5dacb3f6753be14c4fd7fbe404fb60373c1e
Brian Kimbrow <bkimbrow25@gmail.com>
2023-09-04 19:19:23 -0500

    server is running

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-const port = "localhost:3002";
+const express = require('express');
+
+const app = express();
+
+app.get('/', (request, response) => {
+    response.send('Our Server is working');
+}); 
+
+app.listen(3002, () => {
+    console.log(`Starting my port on ${3002}`);
+});
