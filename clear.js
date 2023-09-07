8685f8960efe04dba87410bc50bc474275a97aeb
Brian Kimbrow <bkimbrow25@gmail.com>
2023-09-06 21:14:03 -0500

    clear.js

--- clear.js
+++ clear.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
+const subscription = require("./subscription");
 require("dotenv").config();
 
 
 async function clear() {
     try {
+        await subscription.deleteMany({});
