f550dde7d4edcf28bf598f7a6a2fc951d4f50e12
Gerard <gerard.g.johnson@gmail.com>
2023-09-07 19:52:20 -0500

    add changes js

--- subscription.js
+++ subscription.js
@@ -1,18 +1,17 @@
 const mongoose = require("mongoose");
 
 const Schema = new mongoose.Schema({
-    Title: {
-        type: String, 
-    },
-    Nutrition: {
-        type: String,
-    }, 
-    Days:{
-        type: String,
-    },
+  Title: {
+    type: String,
+  },
+  Nutrition: {
+    type: String,
+  },
+  Days: {
+    type: String,
+  },
 });
 
-const subscription = mongoose.model('subscription', Schema);
-
+const subscription = mongoose.model("subscription", Schema);
 
 module.exports = subscription;
